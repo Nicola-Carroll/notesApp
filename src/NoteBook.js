1d9b156a457ac92c60463bbfdf2a9b667dd1e1c9
moby-codes <mubashar.jaffri@gmail.com>
2021-09-07 17:12:00 +0100

    truncatedNotes method is now working and displaying first 20 chars of every element of the array1

--- src/NoteBook.js
+++ src/NoteBook.js
@@ -4,11 +4,6 @@ class Notebook {
     this.listOfNotes = ['I need to go and buy a football', 'I want to go bowling', 'go and code'];
   
   }
-  // truncatedNote(note){
-//   //we need to split a string element and then display on the first three element of the array
-//   this.listOfNotes.push(note) // this is pushing correctly to the array
-//   return this.listOfNotes;
-// }
 
   truncatedNotes(){
       for (const noteIndex of this.listOfNotes) {
@@ -21,8 +16,8 @@ class Notebook {
     }
   }
 //note.content
-openNotes(){
-  console.log('this prints out')
+openNotes(x){
+  //console.log('this prints out')
  }
 
 }
