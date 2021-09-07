c5fd8526600922ba820eacfc3a6e4bb83320231a
moby-codes <mubashar.jaffri@gmail.com>
2021-09-07 17:09:12 +0100

    truncatedNotes method is now working and displaying first 20 chars of every element of the array

--- src/NoteBook.js
+++ src/NoteBook.js
@@ -1,29 +1,27 @@
 class Notebook {
 
-constructor() {
-  this.listOfNotes = ['I need to go and buy a football', 'I want to go bowling', 'go and code'];
-  //this.truncatedNotes = [];
-
-}
-
-// truncatedNote(note){
+  constructor(listOfNotes) {
+    this.listOfNotes = ['I need to go and buy a football', 'I want to go bowling', 'go and code'];
+  
+  }
+  // truncatedNote(note){
 //   //we need to split a string element and then display on the first three element of the array
 //   this.listOfNotes.push(note) // this is pushing correctly to the array
 //   return this.listOfNotes;
 // }
 
-truncatedNotes(){
-  // dont need the if statement. - dont need the if else statement
-  if(this.listOfNotes.length > 20) { // rewrite using the map function and reiterate through each note in the array
-    return this.listOfNotes.substring(0, 20)+'...'
-  } else if (this.listOfNotes.length === 20 ){ //<=
-    return this.listOfNotes.substring(0, 20)+'...'
-  } else {
-    return listOfNotes;
+  truncatedNotes(){
+      for (const noteIndex of this.listOfNotes) {
+          if(noteIndex.length > 20) { 
+              return noteIndex.substring(0, 20)+'...'
+      }else if(noteIndex.length <= 20) {
+          return noteIndex
+      }
+      
+    }
   }
-}
 //note.content
- openNotes(){
+openNotes(){
   console.log('this prints out')
  }
 
