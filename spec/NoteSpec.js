39219d54dfcde20ec2197e3a4c8ced4160ed7250
Nicola-Carroll <nicolaccarroll@gmail.com>
2021-09-10 15:13:44 +0100

    one small test

--- spec/NoteSpec.js
+++ spec/NoteSpec.js
@@ -1,1 +1,4 @@
-const note = new Note("Note goes here :smile: :heart:");
+const note = new Note("Note goes here :smile:, :heart:!");
+
+note.createWithEmojis();
+expect(note.content).toEqual("Note goes here :smile:, :heart:!");
